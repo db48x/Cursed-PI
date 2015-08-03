adc3618b8d34d27e426411721b63b5357553b09f
L0771 <diego91_hc@hotmail.com>
2015-08-03 02:06:59 -0300

    v0.0.3

--- control.lua
+++ control.lua
@@ -1,5 +1,18 @@
 require("defines")
 
+local posOut =
+{
+	{ 1,2,3,4,5,6,7,8,9 },
+	{ 1,2,3,4,5,6,7,8,9 },
+	{ 7,4,1,8,5,2,9,6,3 },
+	{ 3,6,9,2,5,8,1,4,7 },
+	{ },
+	{ 7,4,1,8,5,2,9,6,3 },
+	{ 3,6,9,2,5,8,1,4,7 },
+	{ 9,8,7,6,5,4,3,2,1 },
+	{ 9,8,7,6,5,4,3,2,1 },
+}
+local dir = { 0,0,2,6,5,2,6,4,4 }
 
 game.on_init(function()
 	for k,force in pairs(game.forces) do
@@ -13,42 +26,32 @@ game.on_init(function()
 			force.technologies["cursed-logistics"].enabled = true
 		end
 	end
-	if global.cursedPI == nil then
-		global.cursedPI = {}
-	end
+	fillGlobal()
+end)
+
+
+game.on_load(function()
+	fillGlobal()
 end)
 
 game.on_event(defines.events.on_built_entity, function(event)
-	if event.created_entity.name == "cursed-burner-inserter" then
-		showGui(event)
-	elseif event.created_entity.name == "cursed-basic-inserter" then
-		showGui(event)
-	elseif event.created_entity.name == "cursed-long-handed-inserter" then
-		showGui(event)
-	elseif event.created_entity.name == "cursed-smart-inserter" then
-		showGui(event)
-	elseif event.created_entity.name == "cursed-fast-inserter" then
-		showGui(event)
+	if event.created_entity.name == "cursed-pa" then
+		if game.get_player(event.player_index).selected ~= nil and game.get_player(event.player_index).selected.type == "assembling-machine" then
+			showGuiAssembler(event)
+		end
+		game.get_player(event.player_index).insert({name="cursed-pa",count = 1})
+		event.created_entity.destroy()
+	elseif global.cursedPIconfig[event.created_entity.name] ~= nil then
+	local player = game.get_player(event.player_index)
+		showGui(event,player)
 	end
 end)
 
 
 game.on_event(defines.events.on_gui_click, function(event)
 	if string.sub(event.element.name,1,8) == "datosPI_" then
-		if event.element.parent.name == "tablePI1" then
-			local active = tonumber(string.sub(event.element.name,-1))
-			for i = 1,9 do
-				if i ~= 5 and i ~= active then
-					event.element.parent["datosPI_" .. i].state = false
-				end
-			end
-		elseif event.element.parent.name == "tablePI2" then
-			local active = tonumber(string.sub(event.element.name,-1))
-			for i = 1,9 do
-				if i ~= 5 and i ~= active then
-					event.element.parent["datosPI_" .. i].state = false
-				end
-			end
+		if event.element.parent.name == "tablePI1" or event.element.parent.name == "tablePI2" then
+			valButtons(event)
 		elseif event.element.name == "datosPI_Accept" then
 			local parent = event.element.parent
 			local valueIn
@@ -69,125 +72,146 @@ game.on_event(defines.events.on_gui_click, function(event)
 			end
 			if valueIn ~= nil and valueOut ~= nil and valueIn ~= valueOut then
 				local inserter = global.cursedPI[event.player_index]
-				inserter.surface.create_entity{name=inserter.name .. "_" .. valueIn .. "_" .. valueOut,position = inserter.position,force = inserter.force}
+				local posx = (valueIn % 2) * -1 + 2
+				local posy = posOut[valueIn][valueOut]
+				inserter.surface.create_entity{name=inserter.name .. "_" .. posx .. "_" .. posy,position = inserter.position,force = inserter.force,direction = dir[valueIn]}
 				inserter.destroy()
 			end
 			global.cursedPI[event.player_index] = nil
 			parent.parent.destroy()
 		end
+	elseif string.sub(event.element.name,1,8) == "datosPA_" then
+		local player = game.get_player(event.player_index)
+		if event.element.parent.name == "tablePA" then
+			showGui(nil,player,string.sub(event.element.name,9))
+		elseif event.element.name == "datosPA_Accept" then
+			local parent = event.element.parent
+			local valueIn
+			local valueOut 
+			for i = 1,9 do
+				if i ~= 5 then
+					if parent["tablePI1"]["datosPI_" .. i].state == true then
+						valueIn = i
+					end
+				end
+			end
+			for i = 1,9 do
+				if i ~= 5 then
+					if parent["tablePI2"]["datosPI_" .. i].state == true then
+						valueOut = i
+					end
+				end
+			end
+			if valueIn ~= nil and valueOut ~= nil and valueIn ~= valueOut then
+				local inserter = global.cursedPI[event.player_index].inserter
+				local assembler = global.cursedPI[event.player_index].assembler
+				local posx = (valueIn % 2) * -1 + 2
+				local posy = posOut[valueIn][valueOut]
+				assembler[1].recipe = player.force.recipes[inserter .. "_" .. posx .. "_" .. posy]
+			end
+			global.cursedPI[event.player_index] = nil
+			parent.parent.destroy()
+		elseif event.element.name == "datosPA_Cancel" then
+			if player.gui.center.framePI ~= nil then
+				player.gui.center.framePI.destroy()
+			end
+			global.cursedPI[player.index] = nil
+		end
 	end
 end)
 
-function showGui(event)
-	local player = game.get_player(event.player_index)
-	global.cursedPI[player.index] = event.created_entity
+function showGui(event,player,button_sel)
+	if player.gui.center.framePI ~= nil then
+		player.gui.center.framePI.destroy()
+	end
+	if event ~= nil then
+		global.cursedPI[player.index] = event.created_entity
+	else
+		global.cursedPI[player.index].inserter = button_sel
+	end
 	local framePI = player.gui.center.add({ type="frame", name="framePI", direction="vertical" })
-	framePI.add({ type="label", name="lbl1",caption={"entity-name."..event.created_entity.name} })
+	if event ~= nil then
+		framePI.add({ type="label", name="lbl1",caption={"entity-name."..event.created_entity.name} })
+	else
+		framePI.add({ type="label", name="lbl1",caption={"entity-name."..button_sel} })
+	end
 	local tablePI = framePI.add({ type="table", name="tablePI", colspan = 3,style="cursed-PI-table" })
-	local tablePI1 = tablePI.add({ type="table", name="tablePI1", colspan = 5,style="cursed-PI-table" })
+	local tablePI1 = tablePI.add({ type="table", name="tablePI1", colspan = 3,style="cursed-PI-table" })
 	tablePI.add({ type="label", name="lbl2",caption=" --> " })
-	local tablePI2 = tablePI.add({ type="table", name="tablePI2", colspan = 5,style="cursed-PI-table" })
-	makeTable(tablePI1,event.created_entity.name)
-	makeTable(tablePI2,event.created_entity.name)
+	local tablePI2 = tablePI.add({ type="table", name="tablePI2", colspan = 3,style="cursed-PI-table" })
+	makeTable(tablePI1)
+	makeTable(tablePI2)
 	tablePI.add({ type="label", name="lbl3",caption=" " })
 	tablePI.add({ type="label", name="lbl4",caption=" " })
 	tablePI.add({ type="label", name="lbl5",caption=" " })
-	tablePI.add({ type="button", name="datosPI_Accept", caption = "Accept",style="button_style" })
+	if event ~= nil then
+		tablePI.add({ type="button", name="datosPI_Accept", caption = "Accept",style="button_style" })
+	elseif button_sel ~= nil then
+		tablePI.add({ type="button", name="datosPA_Accept", caption = "Accept",style="button_style" })
+	end
+end
+
+function showGuiAssembler(event)
+	local player = game.get_player(event.player_index)
+	if player.gui.center.framePI ~= nil then
+		player.gui.center.framePI.destroy()
+	end
+	global.cursedPI[player.index] = {}
+	global.cursedPI[player.index].assembler = event.created_entity.surface.find_entities_filtered({area={{math.floor(event.created_entity.position.x),math.floor(event.created_entity.position.y)},{math.ceil(event.created_entity.position.x),math.ceil(event.created_entity.position.y)}},type = "assembling-machine"})
+	local framePI = player.gui.center.add({ type="frame", name="framePI", direction="vertical" })
+	framePI.add({ type="label", name="lbl1",caption={"gui-assembling-machine.choose-recipe"} })
+	local tablePA = framePI.add({ type="table", name="tablePA", colspan = 1,style="cursed-PI-table" })
+	
+	local del = true
+	for k,v in pairs(global.cursedPIconfig) do
+		if player.force.recipes[k.recipe].enabled == true then
+			tablePA.add({ type="button", name="datosPA_" .. k.recipe, caption = {"entity-name." .. v.entity},style="cursed-PI-button" })
+			del = false
+		end
+	end
+	
+	framePI.add({ type="button", name="datosPA_Cancel", caption = "Cancel",style="button_style" })
+	if del == true then
+		player.gui.center.framePI.destroy()
+	end
 end
 
 function makeTable(parent,type)
 
-	if type == "cursed-long-handed-inserter" then
-		parent.add({ type="checkbox", name="datosPI_1",state = false, style = "cursed-PI-" .. type .. "_hand" })
-	else
-		parent.add({ type="checkbox", name="framePI11", state = false, style = "cursed-PI-frame" })
-	end
-	parent.add({ type="checkbox", name="framePI21", state = false, style = "cursed-PI-frame" })
-	if type == "cursed-long-handed-inserter" then
-		parent.add({ type="checkbox", name="datosPI_2",state = false, style = "cursed-PI-" .. type .. "_hand" })
-	else
-		parent.add({ type="checkbox", name="framePI31", state = false, style = "cursed-PI-frame" })
-	end
-	parent.add({ type="checkbox", name="framePI41", state = false, style = "cursed-PI-frame" })
-	if type == "cursed-long-handed-inserter" then
-		parent.add({ type="checkbox", name="datosPI_3",state = false, style = "cursed-PI-" .. type .. "_hand" })
-	else
-		parent.add({ type="checkbox", name="framePI51", state = false, style = "cursed-PI-frame" })
-	end
+	parent.add({ type="checkbox", name="datosPI_1",state = false, style = "cursed-PI_hand" })
+	parent.add({ type="checkbox", name="datosPI_2",state = false, style = "cursed-PI_hand" })
+	parent.add({ type="checkbox", name="datosPI_3",state = false, style = "cursed-PI_hand" })
+	parent.add({ type="checkbox", name="datosPI_4",state = false, style = "cursed-PI_hand" })
+	parent.add({ type="checkbox", name="framePI_5",state = false, style = "cursed-PI_base" })
+	parent.add({ type="checkbox", name="datosPI_6",state = false, style = "cursed-PI_hand" })
+	parent.add({ type="checkbox", name="datosPI_7",state = false, style = "cursed-PI_hand" })
+	parent.add({ type="checkbox", name="datosPI_8",state = false, style = "cursed-PI_hand" })
+	parent.add({ type="checkbox", name="datosPI_9",state = false, style = "cursed-PI_hand" })
 	
-	parent.add({ type="checkbox", name="framePI12", state = false, style = "cursed-PI-frame" })
-	if type == "cursed-long-handed-inserter" then
-		parent.add({ type="checkbox", name="framePI22", state = false, style = "cursed-PI-frame" })
-	else
-		parent.add({ type="checkbox", name="datosPI_1",state = false, style = "cursed-PI-" .. type .. "_hand" })
-	end
-	if type == "cursed-long-handed-inserter" then
-		parent.add({ type="checkbox", name="framePI32", state = false, style = "cursed-PI-frame" })
-	else
-		parent.add({ type="checkbox", name="datosPI_2",state = false, style = "cursed-PI-" .. type .. "_hand" })
-	end
-	if type == "cursed-long-handed-inserter" then
-		parent.add({ type="checkbox", name="framePI42", state = false, style = "cursed-PI-frame" })
-	else
-		parent.add({ type="checkbox", name="datosPI_3",state = false, style = "cursed-PI-" .. type .. "_hand" })
-	end
-	parent.add({ type="checkbox", name="framePI52", state = false, style = "cursed-PI-frame" })
-	
-	if type == "cursed-long-handed-inserter" then
-		parent.add({ type="checkbox", name="datosPI_4",state = false, style = "cursed-PI-" .. type .. "_hand" })
-	else
-		parent.add({ type="checkbox", name="framePI13", state = false, style = "cursed-PI-frame" })
-	end
-	if type == "cursed-long-handed-inserter" then
-		parent.add({ type="checkbox", name="framePI23", state = false, style = "cursed-PI-frame" })
-	else
-		parent.add({ type="checkbox", name="datosPI_4",state = false, style = "cursed-PI-" .. type .. "_hand" })
-	end
-	parent.add({ type="checkbox", name="framePI33",state = false, style = "cursed-PI-" .. type .. "_base" })
-	if type == "cursed-long-handed-inserter" then
-		parent.add({ type="checkbox", name="framePI43", state = false, style = "cursed-PI-frame" })
-	else
-		parent.add({ type="checkbox", name="datosPI_6",state = false, style = "cursed-PI-" .. type .. "_hand" })
-	end
-	if type == "cursed-long-handed-inserter" then
-		parent.add({ type="checkbox", name="datosPI_6",state = false, style = "cursed-PI-" .. type .. "_hand" })
-	else
-		parent.add({ type="checkbox", name="framePI53", state = false, style = "cursed-PI-frame" })
-	end
-	
-	parent.add({ type="checkbox", name="framePI14", state = false, style = "cursed-PI-frame" })
-	if type == "cursed-long-handed-inserter" then
-		parent.add({ type="checkbox", name="framePI24", state = false, style = "cursed-PI-frame" })
-	else
-		parent.add({ type="checkbox", name="datosPI_7",state = false, style = "cursed-PI-" .. type .. "_hand" })
-	end
-	if type == "cursed-long-handed-inserter" then
-		parent.add({ type="checkbox", name="framePI34", state = false, style = "cursed-PI-frame" })
-	else
-		parent.add({ type="checkbox", name="datosPI_8",state = false, style = "cursed-PI-" .. type .. "_hand" })
-	end
-	if type == "cursed-long-handed-inserter" then
-		parent.add({ type="checkbox", name="framePI44", state = false, style = "cursed-PI-frame" })
-	else
-		parent.add({ type="checkbox", name="datosPI_9",state = false, style = "cursed-PI-" .. type .. "_hand" })
-	end
-	parent.add({ type="checkbox", name="framePI54", state = false, style = "cursed-PI-frame" })
+end
 
-	if type == "cursed-long-handed-inserter" then
-		parent.add({ type="checkbox", name="datosPI_7",state = false, style = "cursed-PI-" .. type .. "_hand" })
-	else
-		parent.add({ type="checkbox", name="framePI15", state = false, style = "cursed-PI-frame" })
+function fillGlobal()
+	if global.cursedPI == nil then
+		global.cursedPI = {}
 	end
-	parent.add({ type="checkbox", name="framePI25", state = false, style = "cursed-PI-frame" })
-	if type == "cursed-long-handed-inserter" then
-		parent.add({ type="checkbox", name="datosPI_8",state = false, style = "cursed-PI-" .. type .. "_hand" })
-	else
-		parent.add({ type="checkbox", name="framePI35", state = false, style = "cursed-PI-frame" })
-	end
-	parent.add({ type="checkbox", name="framePI45", state = false, style = "cursed-PI-frame" })
-	if type == "cursed-long-handed-inserter" then
-		parent.add({ type="checkbox", name="datosPI_9",state = false, style = "cursed-PI-" .. type .. "_hand" })
-	else
-		parent.add({ type="checkbox", name="framePI55", state = false, style = "cursed-PI-frame" })
+	if global.cursedPIconfig == nil then 
+		global.cursedPIconfig =
+		{
+			["cursed-burner-inserter"] = { ["entity"] = "cursed-burner-inserter", ["recipe"] = "cursed-burner-inserter" },
+			["cursed-basic-inserter"] = { ["entity"] = "cursed-basic-inserter", ["out"] = "cursed-basic-inserter" },
+			["cursed-long-handed-inserter"] = { ["entity"] = "cursed-long-handed-inserter", ["out"] = "cursed-long-handed-inserter" },
+			["cursed-smart-inserter"] = { ["entity"] = "cursed-smart-inserter", ["out"] = "cursed-smart-inserter" },
+			["cursed-fast-inserter"] = { ["entity"] = "cursed-fast-inserter", ["out"] = "cursed-fast-inserter" }
+		}
 	end
 end
+
+function valButtons(event)
+	local active = tonumber(string.sub(event.element.name,-1))
+	for i = 1,9 do
+		if i ~= 5 and i ~= active then
+			event.element.parent["datosPI_" .. i].state = false
+		end
+	end
+end
+
--- data.lua
+++ data.lua
@@ -1,118 +1,131 @@
 
-function addIconsButtons_hand(name,icon)
-	data.raw["gui-style"]["default"]["cursed-PI-" .. name] =
-	{
-		type = "checkbox_style",
-		font = "default",
-		font_color = {r=1, g=1, b=1},
-		width = 34,
-		height = 34,
-		default_background =
-		{
-			filename = "__Cursed-PI__/graphics/icons/gui/gui1.png",
-			--priority = "extra-high-no-scale",
-			width = 32,
-			height = 32,
-		},
-		hovered_background =
-		{
-			filename = "__Cursed-PI__/graphics/icons/gui/gui1.png",
-			--priority = "extra-high-no-scale",
-			width = 32,
-			height = 32
-		},
-		clicked_background =
-		{
-			filename = "__Cursed-PI__/graphics/icons/gui/gui1.png",
-			--priority = "extra-high-no-scale",
-			width = 32,
-			height = 32
-		},
-		checked =
-		{
-			filename = icon,
-			--priority = "extra-high-no-scale",
-			width = 32,
-			height = 32
-		}
-	}
+if data.raw["item-group"]["cursed-noshow"] == nil then
+	local obj = util.table.deepcopy(data.raw["item-group"]["other"])
+	obj.name = "cursed-noshow"
+	obj.icon = "__Cursed-PI__/graphics/icons/group/group.png"
+	data.raw[obj.type][obj.name] = obj
 end
 
-function addIconsButtons_base(prototype)
-	data.raw["gui-style"]["default"]["cursed-PI-" .. prototype.name .. "_base"] =
-	{
-		type = "checkbox_style",
-		font = "default",
-		font_color = {r=1, g=1, b=1},
-		width = 34,
-		height = 34,
-		default_background =
-		{
-			filename = prototype.icon,
-			--priority = "extra-high-no-scale",
-			width = 32,
-			height = 32,
-		},
-		hovered_background =
-		{
-			filename = prototype.icon,
-			--priority = "extra-high-no-scale",
-			width = 32,
-			height = 32
-		},
-		clicked_background =
-		{
-			filename = prototype.icon,
-			--priority = "extra-high-no-scale",
-			width = 32,
-			height = 32
-		},
-		checked =
-		{
-			filename = prototype.icon,
-			--priority = "extra-high-no-scale",
-			width = 32,
-			height = 32
-		}
-	}
-end
+local obj = util.table.deepcopy(data.raw["item-subgroup"]["other"])
+obj.name = "cursed-noshow-inserter"
+obj.group = "cursed-noshow"
+obj.order = "5"
+data.raw[obj.type][obj.name] = obj
 
-	data.raw["gui-style"]["default"]["cursed-PI-frame"] =
-	{
-		type = "checkbox_style",
-		font = "default",
-		font_color = {r=1, g=1, b=1},
-		width = 34,
-		height = 34,
-		default_background =
-		{
-			filename = "__Cursed-PI__/graphics/icons/gui/gui3.png",
-			--priority = "extra-high-no-scale",
-			width = 32,
-			height = 32,
-		},
-		hovered_background =
-		{
-			filename = "__Cursed-PI__/graphics/icons/gui/gui3.png",
-			--priority = "extra-high-no-scale",
-			width = 32,
-			height = 32
-		},
-		clicked_background =
-		{
-			filename = "__Cursed-PI__/graphics/icons/gui/gui3.png",
-			--priority = "extra-high-no-scale",
-			width = 32,
-			height = 32
-		},
-		checked =
-		{
-			filename = "__Cursed-PI__/graphics/icons/gui/gui3.png",
-			--priority = "extra-high-no-scale",
-			width = 32,
-			height = 32
-		}
-	}
+-- function addIconsButtons_hand(name,icon)
+	-- data.raw["gui-style"]["default"]["cursed-PI-" .. name] =
+	-- {
+		-- type = "checkbox_style",
+		-- font = "default",
+		-- font_color = {r=1, g=1, b=1},
+		-- width = 34,
+		-- height = 34,
+		-- default_background =
+		-- {
+			-- filename = "__Cursed-PI__/graphics/icons/gui/gui1.png",
+			-- --priority = "extra-high-no-scale",
+			-- width = 32,
+			-- height = 32,
+		-- },
+		-- hovered_background =
+		-- {
+			-- filename = "__Cursed-PI__/graphics/icons/gui/gui1.png",
+			-- --priority = "extra-high-no-scale",
+			-- width = 32,
+			-- height = 32
+		-- },
+		-- clicked_background =
+		-- {
+			-- filename = "__Cursed-PI__/graphics/icons/gui/gui1.png",
+			-- --priority = "extra-high-no-scale",
+			-- width = 32,
+			-- height = 32
+		-- },
+		-- checked =
+		-- {
+			-- filename = icon,
+			-- --priority = "extra-high-no-scale",
+			-- width = 32,
+			-- height = 32
+		-- }
+	-- }
+-- end
+
+-- function addIconsButtons_base(prototype)
+	-- data.raw["gui-style"]["default"]["cursed-PI-" .. prototype.name .. "_base"] =
+	-- {
+		-- type = "checkbox_style",
+		-- font = "default",
+		-- font_color = {r=1, g=1, b=1},
+		-- width = 34,
+		-- height = 34,
+		-- default_background =
+		-- {
+			-- filename = prototype.icon,
+			-- --priority = "extra-high-no-scale",
+			-- width = 32,
+			-- height = 32,
+		-- },
+		-- hovered_background =
+		-- {
+			-- filename = prototype.icon,
+			-- --priority = "extra-high-no-scale",
+			-- width = 32,
+			-- height = 32
+		-- },
+		-- clicked_background =
+		-- {
+			-- filename = prototype.icon,
+			-- --priority = "extra-high-no-scale",
+			-- width = 32,
+			-- height = 32
+		-- },
+		-- checked =
+		-- {
+			-- filename = prototype.icon,
+			-- --priority = "extra-high-no-scale",
+			-- width = 32,
+			-- height = 32
+		-- }
+	-- }
+-- end
+
+	-- data.raw["gui-style"]["default"]["cursed-PI-frame"] =
+	-- {
+		-- type = "checkbox_style",
+		-- font = "default",
+		-- font_color = {r=1, g=1, b=1},
+		-- width = 34,
+		-- height = 34,
+		-- default_background =
+		-- {
+			-- filename = "__Cursed-PI__/graphics/icons/gui/gui3.png",
+			-- --priority = "extra-high-no-scale",
+			-- width = 32,
+			-- height = 32,
+		-- },
+		-- hovered_background =
+		-- {
+			-- filename = "__Cursed-PI__/graphics/icons/gui/gui3.png",
+			-- --priority = "extra-high-no-scale",
+			-- width = 32,
+			-- height = 32
+		-- },
+		-- clicked_background =
+		-- {
+			-- filename = "__Cursed-PI__/graphics/icons/gui/gui3.png",
+			-- --priority = "extra-high-no-scale",
+			-- width = 32,
+			-- height = 32
+		-- },
+		-- checked =
+		-- {
+			-- filename = "__Cursed-PI__/graphics/icons/gui/gui3.png",
+			-- --priority = "extra-high-no-scale",
+			-- width = 32,
+			-- height = 32
+		-- }
+	-- }
 
 -- function addIconsButtons_base(prototype)
 	-- data.raw["gui-style"]["default"]["cursed-PI-" .. prototype.name .. "_base"] =
@@ -188,34 +201,52 @@ end
 		-- }
 	-- }
 -- }
-data.raw["gui-style"]["default"]["cursed-PI-table"] =
-{
-	type = "table_style",
-	horizontal_spacing = 2,
-	vertical_spacing = 2
-}
+-- data.raw["gui-style"]["default"]["cursed-PI-table"] =
+-- {
+	-- type = "table_style",
+	-- horizontal_spacing = 2,
+	-- vertical_spacing = 2
+-- }
 
 
 		--[equipment]--
 require("prototypes.entities.cursed-pi")
+require("prototypes.entities.cursed-pa")
 
 		--[Items]--
 require("prototypes.items.cursed-pi")
+require("prototypes.items.cursed-pa")
 
 		--[Recipes]--
 require("prototypes.recipes.cursed-pi")
+require("prototypes.recipes.cursed-pa")
 
 		--[Technologies]--
 require("prototypes.technology.cursed-pi")
 
-addIconsButtons_base(data.raw["item"]["cursed-burner-inserter"])
-addIconsButtons_base(data.raw["item"]["cursed-basic-inserter"])
-addIconsButtons_base(data.raw["item"]["cursed-long-handed-inserter"])
-addIconsButtons_base(data.raw["item"]["cursed-smart-inserter"])
-addIconsButtons_base(data.raw["item"]["cursed-fast-inserter"])
+		--[styles]--
+require("prototypes.styles")
 
-addIconsButtons_hand("cursed-burner-inserter_hand","__Cursed-PI__/graphics/icons/burner-inserter/cursed-burner-inserter_hand.png")
-addIconsButtons_hand("cursed-basic-inserter_hand","__Cursed-PI__/graphics/icons/basic-inserter/cursed-basic-inserter_hand.png")
-addIconsButtons_hand("cursed-long-handed-inserter_hand","__Cursed-PI__/graphics/icons/long-handed-inserter/cursed-long-handed-inserter_hand.png")
-addIconsButtons_hand("cursed-smart-inserter_hand","__Cursed-PI__/graphics/icons/smart-inserter/cursed-smart-inserter_hand.png")
-addIconsButtons_hand("cursed-fast-inserter_hand","__Cursed-PI__/graphics/icons/fast-inserter/cursed-fast-inserter_hand.png")
+function cursedPI_addInserter(name,baseRecipe,baseItem,baseEntity,icon,ingredients,ingMult,energy,energyMult,distIn,distOut,othersRecipe,othersItem,othersEntity)
+	cursedPI_addEntity(baseEntity,name,icon,name,energy,energyMult,distIn,distOut,othersEntity)
+	cursedPI_addItem(baseItem,name,icon,name,othersItem)
+	cursedPI_addRecipe(baseRecipe,name,icon,ingredients,ingMult,othersRecipe)
+end
+
+cursedPI_addInserter("cursed-burner-inserter","burner-inserter","burner-inserter","burner-inserter","__Cursed-PI__/graphics/icons/burner-inserter/cursed-burner-inserter.png",nil,3,nil,3,1,1,{enabled = false})
+cursedPI_addInserter("cursed-basic-inserter","basic-inserter","basic-inserter","basic-inserter","__Cursed-PI__/graphics/icons/basic-inserter/cursed-basic-inserter.png",nil,3,nil,3,1,1,{enabled = false})
+cursedPI_addInserter("cursed-long-handed-inserter","long-handed-inserter","long-handed-inserter","long-handed-inserter","__Cursed-PI__/graphics/icons/long-handed-inserter/cursed-long-handed-inserter.png",nil,3,nil,3,2,2,{enabled = false})
+cursedPI_addInserter("cursed-smart-inserter","smart-inserter","smart-inserter","smart-inserter","__Cursed-PI__/graphics/icons/smart-inserter/cursed-smart-inserter.png",nil,3,nil,3,1,1,{enabled = false})
+cursedPI_addInserter("cursed-fast-inserter","fast-inserter","fast-inserter","fast-inserter","__Cursed-PI__/graphics/icons/fast-inserter/cursed-fast-inserter.png",nil,3,nil,3,1,1,{enabled = false})
+
+-- addIconsButtons_base(data.raw["item"]["cursed-burner-inserter"])
+-- addIconsButtons_base(data.raw["item"]["cursed-basic-inserter"])
+-- addIconsButtons_base(data.raw["item"]["cursed-long-handed-inserter"])
+-- addIconsButtons_base(data.raw["item"]["cursed-smart-inserter"])
+-- addIconsButtons_base(data.raw["item"]["cursed-fast-inserter"])
+
+-- addIconsButtons_hand("cursed-burner-inserter_hand","__Cursed-PI__/graphics/icons/burner-inserter/cursed-burner-inserter_hand.png")
+-- addIconsButtons_hand("cursed-basic-inserter_hand","__Cursed-PI__/graphics/icons/basic-inserter/cursed-basic-inserter_hand.png")
+-- addIconsButtons_hand("cursed-long-handed-inserter_hand","__Cursed-PI__/graphics/icons/long-handed-inserter/cursed-long-handed-inserter_hand.png")
+-- addIconsButtons_hand("cursed-smart-inserter_hand","__Cursed-PI__/graphics/icons/smart-inserter/cursed-smart-inserter_hand.png")
+-- addIconsButtons_hand("cursed-fast-inserter_hand","__Cursed-PI__/graphics/icons/fast-inserter/cursed-fast-inserter_hand.png")
